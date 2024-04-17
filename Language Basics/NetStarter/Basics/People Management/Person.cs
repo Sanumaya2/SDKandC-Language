47a087079e29b3920993f108f2fdc948549feed3
Sanumaya2 <goritara23456@gmail.com>
2024-04-16 23:24:23 -0700

    Edit file

--- Language Basics/NetStarter/Basics/People Management/Person.cs
+++ Language Basics/NetStarter/Basics/People Management/Person.cs
@@ -3,9 +3,15 @@ using System.Dynamic;
 public class Person
 {
     public string Index { get; set;}
-  
+
+    public string UserId {get; set;}
     public string FirstName {get; set ;}
     public string LastName {get; set;}
+    public string Sex {get; set;}
+    public string Email {get; set;}
+    public string Phone {get; set;}
+    public string DateofBirth {get; set;}
+    public string JobTitle {get; set;}
    
     
 
