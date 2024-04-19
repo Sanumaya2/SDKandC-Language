fa39ef96b3719eb7a6c771a4833a7448730b5a93
Sanumaya2 <goritara23456@gmail.com>
2024-04-19 10:40:30 -0700

    classwork

--- Language Basics/NetStarter/Basics/LINQ.cs
+++ Language Basics/NetStarter/Basics/LINQ.cs
@@ -6,28 +6,38 @@ using System.Security.AccessControl;
 
 class LINQ
 {
-    int[] ages = [12,45,78,89];
+    int[] ages = [12, 45, 78, 89];
+    List<Student> students = [
+        new Student() { name = "Ram", address = "Ktm"},
+        new Student() { name = "Laxman", address = "Dang"},
+        new Student() { name = "Bikendra", address = "Ktm"},
+        new Student() { name = "Sandip", address = "Dang"},
+        new Student() { name = "Chahana", address = "Dhangadhi"},
+    ];
 // find squares of all numbers in ages collection.
 public void LearnLinq()
-{
-List<int>squares =[];
-foreach(var age in ages)
-{
-    var square = age * age;
-    squares.Add(square);
-}
-//declarative
-// var squares = ages.Select(age => age *age);
-//  var squaresAlternate = from age in ages select age * age;
+    {
+        List<int> squares = [];
+        foreach (var age in ages)
+        {
+            var square = age * age;
+            squares.Add(square);
+        }
+        //declarative
+        // var squares = ages.Select(age => age *age);
+        //  var squaresAlternate = from age in ages select age * age;
 
-//find odd numbers within ages
-    var odds = ages.Where(x => x % 2 == 1);
+        //find odd numbers within ages
+        // var odds = ages.Where(x => x % 2 == 1);
 
-    //find cube of all even numbers within ages collection.
-    var cubesofEvenNumber = ages.Where(age => age % 2 ==0 );
-    var evennumber = ages.Select(age => age * age *age);
-    
-}
+        //find cube of all even numbers within ages collection.
+        // var cubesofEvenNumber = ages.Where(age => age % 2 == 0) .Select(age => age * age * age);
+
+       // find student who lives in dang and sort the result by names.
+       
+       var Dang = students.Where(students => students.address == "Dang") .OrderBy(students => students.Name);
+
+    }
 
 
 }
